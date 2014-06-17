f54ad231fb56d825df740096ce4005b3efec8ed1
Phil Ngo <ngo.phil@gmail.com>
2014-06-17 15:45:16 -0500

    Create a skeletal disaggregation module modeling devices, device types and buildings, with explanatory ipython notebook
    Former-commit-id: 585589b7ecd02b46c4d83293a1457a73ea8fd0db
    Former-commit-id: 67f0aa71374ed3a36d636427940bee93a8b20858

---
+++ disaggregator/disaggregator.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+import pandas as pd
+
+class Device:
+    '''Device
+
+    Attributes:
+        data -- a timeseries of measurements in a pandas dataframe
+        params -- a dict of parameters representing this instance of a device
+    '''
+    # TODO - figure out how to store data efficiently
+
+    def __init__(self, data=None, params={}):
+        self.data = data
+        self.params = params
+
+    def add_data(self, data):
+        # adds new data to old dataset
+        pass
+
+    def get_data(self):
+        return self.data
+
+    def get_parameters(self):
+        return params
+
+    def learn_parameters(self):
+        # procedure to learn parameters a specific device
+        pass
+
+    def update_data(self, data):
+        # replaces data
+        self.data = data
+
+    def update_parameters(self,params):
+        # replaces old parameters with manually updated parameters
+        self.params = params
+
+class DeviceType:
+    '''DeviceType models the general parameters for a set of devices
+
+    Attributes:
+        devices -- list of devices in the device set
+        name    -- name of the device type
+        params  -- set of parameters used to model the device
+    '''
+    # TODO - figure out how to store data efficiently
+
+    def __init__(self, devices=[], name=None, params=None):
+        self.devices = devices
+        self.name = name
+
+    def add_devices(self, devices):
+        self.devices += devices
+
+    def generate_data(self,timeframe):
+        # generates generic data for this device in a given timeframe
+        pass
+
+    def get_devices(self):
+        return devices
+
+    def get_parameters(self):
+        return params
+
+    def learn_parameters(self):
+        pass
+
+    def probability_present(self, data):
+        # evaluates the probability of being present in a particular dataset
+        pass
+
+    def update_devices(self, devices):
+        self.devices = devices
+
+    def update_name(self, name):
+        self.name = name
+
+    def update_parameters(self, params):
+        # manually updates parameters
+        self.params = params
+
+class Building:
+    '''Class to represent a building with known (or unknown) device types
+    '''
+
+    def __init__(self, device_types=[]):
+        self.device_types = device_types
+
+    def add_device_types(self):
+        pass
+
+    def get_device_types(self):
+        return self.device_types
+
+    def learn_device_types(self, data, possible_device_types):
+        # learns which devices are present, retains and returns the devices
+        # which are.
+        pass
+
+    def disaggregate(self, data):
+        # gets unlabeled data, returns labeled data
+        disaggregated_data = None
+        confidences = None
+        return disaggregated_data, confidences
+
+    def update_device_types(self, device_types):
+        self.device_types = device_types
+
+
+class Error(Exception):
+    '''Base class for exceptions in this module.'''
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return repr(self.msg)
+
+class DatasetError(Error):
+    '''Exception raised for errors in the dataset.
+
+    Attributes:
+        msg  -- explanation of the error
+    '''
+    pass
+
+class UndefinedDeviceClassesError(Error):
+    '''Exception raised for errors in the dataset.
+
+    Attributes:
+        msg  -- explanation of the error
+    '''
+    pass
