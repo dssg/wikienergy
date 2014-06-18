27190c6d852234ec010efd7505687e0a31560c27
Phil Ngo <ngo.phil@gmail.com>
2014-06-18 09:16:17 -0500

    disaggregator is abstract class
    Former-commit-id: dbda14e138a2a9cb1a6169103017f971b859a5f4
    Former-commit-id: a4f4ff3e5e2aeedd3dee039098463ecdd5073366

--- disaggregator/disaggregator.py
+++ disaggregator/disaggregator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-import pandas as pd
+from abc import ABCMeta
 
-class DeviceInstance:
+class DeviceInstance(object):
     '''DeviceInstance
 
     Attributes:
         data -- a timeseries of measurements in a pandas dataframe
         params -- a dict of parameters representing this instance of a device
     '''
-    # TODO - figure out how to store data efficiently
+    __metaclass__ = ABCMeta
 
     def __init__(self, traces=[], params={}):
         self.traces = traces
@@ -20,6 +20,7 @@ class DeviceInstance:
     def get_traces(self):
         return self.traces
 
+    @abstractmethod
     def learn_parameters(self):
         # procedure to learn parameters a specific device
         pass
@@ -32,7 +33,7 @@ class DeviceInstance:
         # replaces traces
         self.data = data
 
-class DeviceType:
+class DeviceType(object):
     '''DeviceType models the general parameters for a set of devices
 
     Attributes:
@@ -41,11 +42,14 @@ class DeviceType:
         params  -- set of parameters used to model the device
     '''
     # TODO - figure out how to store data efficiently
+    __metaclass__ = ABCMeta
 
     def __init__(self, devices=[], name=None, params={}):
         self.devices = devices
         self.name = name
+        self.params = params
 
+    @abstractmethod
     def generate_data(self, timeframe):
         # generates generic data for this device within a given timeframe
         pass
@@ -59,40 +63,48 @@ class DeviceType:
     def get_parameters(self):
         return self.params
 
+    @abstractmethod
     def learn_parameters(self):
         pass
 
+    @abstractmethod
     def probability_present(self, aggregated_data):
         # evaluates the probability of being present in a particular timeseries
         pass
 
-    def update_devices(self, devices):
+    def set_devices(self, devices):
         self.devices = devices
 
-    def update_name(self, name):
+    def set_name(self, name):
         self.name = name
 
-    def update_parameters(self, params):
+    def set_parameters(self, params):
         self.params = params
 
-class MeteredUnit:
+class MeteredUnit(object):
     '''Class to represent a metered with known (or unknown) device types
     '''
 
-    def __init__(self, device_types=[]):
+    def __init__(self, device_types=[], params={}):
         self.device_types = device_types
 
     def get_device_types(self):
         return self.device_types
 
+    def get_parameters(self):
+        return self.params
+
+    @abstractmethod
     def learn_device_types(self, data, possible_device_types):
         # learns which devices are present, retains and returns the devices
         # which are.
         pass
 
-    def learn_disaggregator_parameters(self,timeframe=None):
+    @abstractmethod
+    def learn_disaggregator_parameters(self, timeframe=None):
         pass
 
+    @abstractmethod
     def disaggregate(self, aggregated_data):
         # takes in unlabeled data, returns labeled data
         disaggregated_data = None
@@ -102,6 +114,8 @@ class MeteredUnit:
     def set_device_types(self, device_types):
         self.device_types = device_types
 
+    def set_parameters(self, params):
+        self.params = params
 
 class Error(Exception):
     '''Base class for exceptions in this module.'''
