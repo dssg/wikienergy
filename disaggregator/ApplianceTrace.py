5ad0f89174160014e9a84e47fb15825313a355c2
Phil Ngo <ngo.phil@gmail.com>
2014-06-30 17:08:48 -0500

    update appliance trace
    Former-commit-id: d49936b702c084bf13499cca47d7ae079aa5e561 [formerly bd0fb71da9c6b60eb7067c968ee4ee92aeb0f473] [formerly d03fcfa22110d5fab5ec2666868001f099861f91]
    Former-commit-id: 366b8db9c0d69f1291facd747288ce96b559af86
    Former-commit-id: 8dd7c2a7660447a26e8036a4e68c8c318a835828

--- disaggregator/ApplianceTrace.py
+++ disaggregator/ApplianceTrace.py
@@ -1,7 +1,18 @@
 class ApplianceTrace(object):
 
-    def __init__(self, series):
+    def __init__(self, series, source):
+        '''
+        Initializes a trace object from a series and a source.
+        Series must be sampled at a particular sample rate
+        '''
         self.series = series
+        self.source = source
 
     def get_series(self):
         return self.series
+
+    def get_source(self):
+        return self.series
+
+    def get_sampling_rate(self):
+        return self.series.index.freq
