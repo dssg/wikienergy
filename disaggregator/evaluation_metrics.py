891b8792ea27108654423228c1f7cba8670548d8
Sabina321 <sabina@Sabinas-MacBook-Pro.local>
2014-06-30 16:15:24 -0500

    eval and testing
    Former-commit-id: e29ce405b2d614c86bf4348f782739f1d2a4d1af
    Former-commit-id: 989b5d87fb50dbf7220a0144bdb0026cb357829d

--- disaggregator/evaluation_metrics.py
+++ disaggregator/evaluation_metrics.py
@@ -1,5 +1,9 @@
-def evaluation_metric1(truth,prediction): # change these names
-    return ####
+import numpy as np
+import math
+
+def sum_error(truth,prediction):
+    '''For a numpy array of truth values and prediction values returns the difference between their sums'''
+    return math.fabs(truth.sum()-prediction.sum())
 
 def evaluation_metric2(truth,prediction): # change these names
     return ####
