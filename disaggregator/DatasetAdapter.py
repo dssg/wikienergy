b2d08007ae7e3c98f7617e176f2da3f5f90aadfc
Phil Ngo <ngo.phil@gmail.com>
2014-06-30 13:26:40 -0500

    add dataset adapter class
    Former-commit-id: 1c6bc817c1d83c390ebcc49f0b9de0e5c1812fbe [formerly ca46881eb49f971b6e6e69da8861cfa318acd51d] [formerly 3b0b91d4e54a50cf939f3dd5ea6e46aeef2b3e6d]
    Former-commit-id: fa20bb620d844b1cbfe8e286fed19c091626bffa
    Former-commit-id: c8482bd22637e1830c95980c064fc78f1d02fa22

---
+++ disaggregator/DatasetAdapter.py
@@ -0,0 +1,19 @@
+from abc import ABCMeta
+
+class DatasetAdapter(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+
+    def createTrace(self):
+        pass
+
+    def createInstance(self):
+        pass
+
+    def createType(self);
+        pass
+
+    def createSet(self):
+        pass
