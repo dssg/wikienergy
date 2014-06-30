029ca97ea03fb14a230a3e103c5b8e24410650d2
Sabina321 <sabina@Sabinas-MacBook-Pro.local>
2014-06-30 16:55:11 -0500

    added rss
    Former-commit-id: 15693da0d39a58c5f6e36917a3305243228f936e
    Former-commit-id: 9ae9c65616b50052be5e07c8283451fe591c3351

--- disaggregator/evaluation_metrics.py
+++ disaggregator/evaluation_metrics.py
@@ -1,12 +1,15 @@
 import numpy as np
 import math
+import scipy
 
 def sum_error(truth,prediction):
-    '''For a numpy array of truth values and prediction values returns the difference between their sums'''
+    '''For a numpy array of truth values and prediction values returns the absolute value of the difference between their sums'''
     return math.fabs(truth.sum()-prediction.sum())
 
-def evaluation_metric2(truth,prediction): # change these names
-    return ####
+def rss(truth,prediction):
+    '''sum of squared residuals'''
+    
+    return np.sum(np.square(np.subtract(truth,prediction)))
 
 def evaluate(truth,prediction):
    pass # run all metrics
