0fa623081da68c2bacd4aca047b11e586387dee8
Sabina321 <sabina@Sabinas-MacBook-Pro.local>
2014-06-30 16:55:11 -0500

    added rss
    Former-commit-id: ad4dfb3c915d13c7d90f4820c4dc932c33eb0e26
    Former-commit-id: a6706d23c4f68831248e5ddfa1c87026cf89188b
    Former-commit-id: 8d3d6fcad467e22d4bc2a045cd952ff1367e0253

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,14 +1,20 @@
 import sys
 sys.path.append('../')
 #print sys.path
+import numpy as np
 
 import numpy as np
 from disaggregator import evaluation_metrics as evm
 
-def main():
+def unit_rss(t,p):
+    residuals = np.array([x-y for (x,y) in zip(t,p)])
+    squares = np.array([x**2 for x in residuals])
+    return squares.sum()
+
+if __name__=='__main__':
     truth = np.ones(7)
     prediction = np.zeros(7)
     print evm.sum_error(truth,prediction)
+    print 'The sum of the squared residuals(evm) is {} '.format(evm.rss(truth,prediction))
+    print 'The sum of the squared residuals(unit) is {}'.format(unit_rss(truth,prediction))
 
-if __name__=='__main__':
-    main()
