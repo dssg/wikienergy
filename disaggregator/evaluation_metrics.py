492b2d3b4d90925d6a3c7cdef1024bf733017d9c
Phil Ngo <ngo.phil@gmail.com>
2014-06-30 13:18:43 -0500

    evaluation metrics
    Former-commit-id: 8fe4369c249124bdf8bae311082cae319a308e04
    Former-commit-id: 9e92308628eb5278f5cfb6c17163c63080b992bd

---
+++ disaggregator/evaluation_metrics.py
@@ -0,0 +1,8 @@
+def evaluation_metric1(truth,prediction): # change these names
+    return ####
+
+def evaluation_metric2(truth,prediction): # change these names
+    return ####
+
+def evaluate(truth,prediction):
+   pass # run all metrics
