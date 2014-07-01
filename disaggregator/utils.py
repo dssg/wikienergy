2891c563a4c723c655afc325b798ff649093115f
Phil Ngo <ngo.phil@gmail.com>
2014-07-01 13:15:03 -0500

    add concatenate_traces to utils
    Former-commit-id: 87ca88ef9a843c8350fa07746b2d69ffa234ac20
    Former-commit-id: 19a90e209cb8ea87b8bbc328cbdd709530e2ff38

--- disaggregator/utils.py
+++ disaggregator/utils.py
@@ -2,3 +2,10 @@ from ApplianceTrace import ApplianceTrace
 from ApplianceInstance import ApplianceInstance
 from ApplianceType import ApplianceType
 from ApplianceSet import ApplianceSet
+
+def concatenate_traces(traces):
+    '''
+    Given a list of consecutive appliance traces, returns a single concatenated
+    trace. Must be sampled at the same rate.
+    '''
+    pass
