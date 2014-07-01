18b8bef57e11ababb66962f4e3da561e04b0a340
Sabina321 <sabina@Sabinas-MacBook-Pro.local>
2014-06-30 19:15:36 -0500

    added precision and some comments
    Former-commit-id: 4bf8aabb49b9f1122de013de06fb9fc377e3599b [formerly 4f63c53d9a550d0f655b0a817c5d55434a26b28f] [formerly f473a6eb476be0e5f11949b0e5a38bb521c61a5d]
    Former-commit-id: f9b3879098631953937ce667c7784416feabff42
    Former-commit-id: 471c42cc69c38a573d554620f2294bbec5f49b90

--- disaggregator/evaluation_metrics.py
+++ disaggregator/evaluation_metrics.py
@@ -23,13 +23,21 @@ def get_positive_negative_stats(true_states, predicted_states):
     false_positives = np.array([1 if a==0 and b==1 else 0 for (a,b) in zip(true_states,predicted_states)])
     return [true_positives,false_negatives,true_negatives,false_positives]
 
-def get_specificity(true_positives,false_negatives):
+def get_sensitivity(true_positives,false_negatives):
+    '''Given a numpy array of true positives, and false negatives returns a sensitivty measure. Then the sensitivity is equal to TP/(TP+FN), where TP is a true positive, such that TP=1 when the predicted value was correctly classified as positive and 0 otherwise and FN is false negative, such that FN = 1 if a value was falsely predicted to be negative and 0 otherwise.
+        '''
     return true_positives.sum()/(np.sum(true_positives.sum(),false_negatives.sum()))
 
-def get_sensitivity():
-    pass
-def get_precision():
-    pass
+def get_specificity(true_negatives, false_positives):
+    '''Given a numpy array of true negatives, and false positives returns a specificty measure. The specificity measure is equal to TN/(TN+FP), where TN is a true negative, such that TN=1 when the predicted value was correctly classified as negative and 0 otherwise and FP is a false positive, such that FP = 1 if a value was falsely predicted to be positive and 0 otherwise.
+        '''
+    return true_negatives.sum()/(np.sum(true_negatives.sum(),false_positives.sum()))
+
+def get_precision(true_positives,false_positives):
+    '''Given a numpy array of true positives, and false positives returns a precision measure. The precision measure is equal to TP/(TP+FP), where TP is a true positive, such that TP=1 when the predicted value was correctly classified as positive and 0 otherwise and FP is a false positive, such that FP = 1 if a value was falsely predicted to be positive and 0 otherwise.
+        '''
+    return true_positives.sum()/(np.sum(true_positives.sum(),false_positives.sum()))
+
 
 def get_accuracy():
     pass
