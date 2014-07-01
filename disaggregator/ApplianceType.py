7eb9ffc172fc8108d9af0068087421aa6ee73291
Phil Ngo <ngo.phil@gmail.com>
2014-07-01 09:12:19 -0500

    update appliance types
    Former-commit-id: 32ed254feb5ea8338fe59a286064e5bac1fa4d1e
    Former-commit-id: b03980f86859d9fbd719ab7ad0651f2ce3da8715

--- disaggregator/ApplianceType.py
+++ disaggregator/ApplianceType.py
@@ -1,4 +1,31 @@
 class ApplianceType(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, instances):
+        '''
+        Initialize a type object with a list of instances. (Check for
+        uniqueness?)
+        '''
+        # TODO Check for uniqueness?
+        self.instances = instances
+
+    def add_instances(self,instances):
+        '''
+        Add instances to the list of instances. (Check for uniqueness?)
+        '''
+        # TODO Check for uniqueness?
+        self.instances += instances
+
+    def get_instances(self):
+        '''
+        Returns the list of appliance instances which are members of this type.
+        '''
+        return self.instances
+
+    def set_instances(self,instances):
+        '''
+        Replaces the old list of instances with the new set of instances.
+        (Check for uniqueness?)
+        '''
+        # TODO Check for uniqueness?
+        self.instances = instances
+
