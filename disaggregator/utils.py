09a51ce1e303e625a9613452a529b390e2c110ea
Phil Ngo <ngo.phil@gmail.com>
2014-07-02 16:13:10 +0000

    update utils comcatenate function
    Former-commit-id: b89f8ec211ace7f0de2d0c0bf228fad00209a157 [formerly 98d33b515308de45d48c205309e9735856e8012b] [formerly 400a4a7820b364db63e5ffc3daaf4b8867f28ff5]
    Former-commit-id: 68932c3b6e6644474b6bb095b0d4b992298b4dd0
    Former-commit-id: 21bcd6a1eb48265850d24af9e301781848475987

--- disaggregator/utils.py
+++ disaggregator/utils.py
@@ -2,10 +2,20 @@ from ApplianceTrace import ApplianceTrace
 from ApplianceInstance import ApplianceInstance
 from ApplianceType import ApplianceType
 from ApplianceSet import ApplianceSet
+from pandas import concat
 
-def concatenate_traces(traces):
+def concatenate_traces(traces, metadata=None, how="strict"):
     '''
-    Given a list of consecutive appliance traces, returns a single concatenated
-    trace. Must be sampled at the same rate.
+    Given a list of appliance traces, returns a single concatenated
+    trace. With how="strict" option, must be sampled at the same rate and
+    consecutive, without overlapping datapoints.
     '''
-    pass
+    if not metadata:
+        metadata = traces[0].metadata
+
+    if how == "strict":
+        # require ordered list of consecutive, similarly sampled traces with no
+        # missing data.
+        return ApplianceTrace(concat([t.series for t in traces],metadata))
+    else:
+        raise NotImplementedError
