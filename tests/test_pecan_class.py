8661300a727d59977ec4243cc3838b3685aa9c80
Phil Ngo <ngo.phil@gmail.com>
2014-07-02 17:30:42 -0500

    add simple test cases for appliance trace
    Former-commit-id: 69f256a539ee7146dcc1911a4addc34a501b97ff
    Former-commit-id: c165d82fc09eecb370b9cc31bcab884eb23ff74b

--- tests/test_pecan_class.py
+++ tests/test_pecan_class.py
@@ -1,17 +1,29 @@
 import sys
 sys.path.append('../')
-#print sys.path
 from disaggregator import PecanStreetDatasetAdapter as pecan
 
-db_url = "postgresql://USERNAME:PASSWORD@db.wiki-energy.org:5432/postgres"
+import unittest
 
-p = pecan(db_url)
-tables = p.get_table_names('shared')
-print tables
 
-i,a = p.get_table_metadata('shared',str(tables[0]))
-#apps = p.get_meta_table('\"PecanStreet_SharedData\"',str(tables[0]))
-#traces = p.get_month_traces('SharedData',tables[0])
-#print a
-trace = p.get_month_traces_wo_time_align('shared',str(tables[0]),i[0])
-#p.get_app_traces('shared',str(tables[0]),'refrigerator1')
+class PecanStreetDatasetAdapterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        db_url = "postgresql://USERNAME:PASSWORD@db.wiki-energy.org:5432/postgres"
+        self.p = pecan(db_url)
+
+    def test_get_table_names(self):
+        #tables = self.p.get_table_names('shared')
+        pass
+
+    def test_table_metadata(self):
+        #tables = self.p.get_table_names('shared')
+        #i,a = p.get_table_metadata('shared',str(tables[0]))
+        pass
+
+    def test_get_month_traces(self):
+        #traces = p.get_month_traces('SharedData',tables[0])
+        #trace = p.get_month_traces_wo_time_align('shared',str(tables[0]),i[0])
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
