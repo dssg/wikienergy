fcd1e102e1b14fb4e617040cc482b5ae9c6c2089
Phil Ngo <ngo.phil@gmail.com>
2014-07-02 17:05:41 -0500

    Update unittesting framework
    Former-commit-id: 77f1129897ebffc9bf113280c583886edde28abd
    Former-commit-id: 2c14a009f6dce126c090cb2ce6d86bd8e32b8d23

--- tests/test_pecan_class.py
+++ tests/test_pecan_class.py
@@ -3,19 +3,15 @@ sys.path.append('../')
 #print sys.path
 from disaggregator import PecanStreetDatasetAdapter as pecan
 
-user_name = 'stomkins'
-pw='PASSWORD'
-host = "db.wiki-energy.org"
-port = "5432"
-db = "postgres"
-db_url = "postgresql"+"://"+user_name+":"+pw+"@"+host+":"+port+"/"+db
+db_url = "postgresql://USERNAME:PASSWORD@db.wiki-energy.org:5432/postgres"
 
 p = pecan(db_url)
-tables= p.set_table_names('PecanStreet_SharedData')
+tables = p.get_table_names('shared')
+print tables
 
-[i,a] = p.get_meta_table('\"PecanStreet_SharedData\"',str(tables[0]))
+i,a = p.get_table_metadata('shared',str(tables[0]))
 #apps = p.get_meta_table('\"PecanStreet_SharedData\"',str(tables[0]))
 #traces = p.get_month_traces('SharedData',tables[0])
 #print a
-trace = p.get_month_traces_wo_time_align('\"PecanStreet_SharedData\"',str(tables[0]),i[0])
-#p.get_app_traces('\"PecanStreet_SharedData\"',str(tables[0]),'refrigerator1')
+trace = p.get_month_traces_wo_time_align('shared',str(tables[0]),i[0])
+#p.get_app_traces('shared',str(tables[0]),'refrigerator1')
