768f14e53d43f564c4872e5d379f272ad36c8f91
Phil Ngo <ngo.phil@gmail.com>
2014-07-03 11:56:13 -0500

    add aggregate traces to utils
    Former-commit-id: c6bf472ef4ebfe5a5347f31ed39fa3063a504591
    Former-commit-id: 471b4f49df8deb9b84a1f2c6591827f446708d8a
    Former-commit-id: 13f751b5e9466601e88a6678fff8c91066ac7a0e

--- proto/pylearn2/make_ac_existence_dataset.py
+++ proto/pylearn2/make_ac_existence_dataset.py
@@ -3,17 +3,18 @@ sys.path.append('.')
 
 import disaggregator as da
 from pandas.tseries.offsets import DateOffset
+import numpy as np
 
-db_url = "postgresql://USERNAME:PASSWORD@db.wiki-energy.org:5432/postgres"                                                                                         
-ps_adapter = da.PecanStreetDatasetAdapter(db_url)
+db_url = "postgresql://USERNAME:PASSWORD@db.wiki-energy.org:5432/postgres"
+psda = da.PecanStreetDatasetAdapter(db_url)
 
-dataids = ps_adapter.get_unique_dataids("curated",2013,1,group=1)
-traces = ps_adapter.get_month_traces("curated",2013,1,dataids[-1],group=1)
+dataids = psda.get_unique_dataids("curated",2013,1,group=1)
+traces = psda.get_month_traces("curated",2013,1,dataids[-1],group=1)
+instances = [da.ApplianceInstance([trace]) for trace in traces]
+usage_order = np.argsort([i.traces[0].get_total_usage() for i in instances])[::-1]
 
-shift = traces[0].series.size
-print shift
-traces[1].series.index = traces[1].series.index + DateOffset(months=1,days=1)
-print traces[0].series
-print traces[0].series.index[-1] - traces[0].series.index[0] + traces[0].series.index
-print traces[1].series
-#print [float(t.get_total_usage()) for t in traces]
+top_5_instances = [instances[i] for i in usage_order[1:6]]
+
+print [inst.traces[0].series.name for inst in top_5_instances]
+
+print da.aggregate_traces(traces, {"name":"aggregated"}).series
