6bf43087967dee2bfb9f31a5de61c91ed0664586
Phil Ngo <ngo.phil@gmail.com>
2014-07-08 12:37:00 -0500

    update get ids and columns in pecanstreet, much faster
    Former-commit-id: 8329bfcf9425baa3e16605c21e07d8fb989e71f8
    Former-commit-id: 30509ccdecaea12fbb4fed10e52a9e8d9d0dcada

---
+++ proto/pylearn2/create_ev_dataset.py
@@ -0,0 +1,21 @@
+import sys
+import os.path
+sys.path.append(os.path.join(os.pardir,os.pardir))
+import disaggregator as da
+import disaggregator.PecanStreetDatasetAdapter as psda
+
+db_url = "postgresql://USERNAME:PASSWORD@db.wiki-energy.org:5432/postgres"
+psda.set_url(db_url)
+schema = 'shared'
+
+
+table_names = psda.get_table_names(schema)
+
+all_ids = []
+all_columns = []
+for table_name in table_names:
+    ids,columns = psda.get_table_dataids_and_column_names(schema,table_name)
+    all_ids.append(ids)
+    all_columns.append(columns)
+print all_ids
+print all_columns
