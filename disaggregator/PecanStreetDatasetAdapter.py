14b5359eb31618b7773ae1e8927ce89972d450ab
Stephen Suffian <stephen.suffian@gmail.com>
2014-07-08 15:03:44 -0500

    added ppt
    Former-commit-id: 93af965536a935172b4e47951648d09256785f23 [formerly 093d1c4f0076a03ea015352dfc04fa9372f90e1d] [formerly 24232a424434bcb3254bcb89b53119f2fab31cf9]
    Former-commit-id: 927ce7a4064ba3cbf5bb3bfca2730d09a4a9d553
    Former-commit-id: ea489b3c78044976f746236cdd04615e5e21b0bf

--- disaggregator/PecanStreetDatasetAdapter.py
+++ disaggregator/PecanStreetDatasetAdapter.py
@@ -1,6 +1,7 @@
 from appliance import ApplianceTrace
 from appliance import ApplianceInstance
 from appliance import ApplianceSet
+from appliance import ApplianceType
 
 import sqlalchemy
 import pandas as pd
