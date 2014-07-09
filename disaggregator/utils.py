2d31674d7166994be2fea6b18668f0ccad1a7928
Stephen Suffian <stephen.suffian@gmail.com>
2014-07-09 12:46:57 -0500

    changed utils
    Former-commit-id: 1c2ef33eebdeb1d8ed471cdedb516bf7bbe30b78 [formerly 38be7a6b66788466902cb3ce44f66432e0be6594] [formerly 9c98d8f997f1d12cd4537414f3dda9f4b82165a8]
    Former-commit-id: b2d4e8fa5b68de5d890e89ecd2c39b07b5425916
    Former-commit-id: 22d17cc829387ac14bf2bf57d1a947ced2722ecb

--- disaggregator/utils.py
+++ disaggregator/utils.py
@@ -43,6 +43,46 @@ def get_common_ids(id_lists):
         common_ids &= id_set
     return list(common_ids)
 
+
+def split_trace_into_daily(trace):
+    '''
+    Given a single trace, a list of traces are returned that are each
+    from a unique date.
+    '''
+    series_list=None;
+    traces=[]
+    for i,group in enumerate(trace.series.groupby(trace.series.index.date)):
+            metadata=trace.metadata
+            metadata['trace_num']=i
+            traces.append(appliance.ApplianceTrace(group[1],metadata))
+    return traces
+
+def split_instance_traces_into_daily(device_instance):
+    '''
+    Each trace in an instance is split into multiple traces that are each
+    from a unique date
+    '''
+    traces=[]
+    for trace in instance.traces:
+        traces.extend(split_trace_into_daily(trace))
+    device_instance.traces=traces
+    return device_instance
+
+def split_type_traces_into_daily(device_type):
+    '''
+    Each trace in each instance of a type is split into multiple traces 
+    that are each from a unique date
+    '''
+    traces=[]
+    instances=[]
+    for instance in device_type.instances:
+        for trace in instance.traces:
+            traces.extend(split_trace_into_daily(trace))
+        instance.traces=traces
+        instances.append(instance)
+    device_type.instances=instances    
+    return device_type
+
 def concatenate_traces(traces, metadata=None, how="strict"):
     '''
     Given a list of appliance traces, returns a single concatenated
