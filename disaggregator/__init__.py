b36f1e110a7f9a635457ac72bb9e28793483c6d6
Phil Ngo <ngo.phil@gmail.com>
2014-07-09 11:58:02 -0500

    update init file with docstring
    Former-commit-id: e9ed125a4177368f7e019c31f488eebe0404810b [formerly 9e0caf9420cc56c00bebbb8534cff14a5bd03f0b] [formerly 6be913c85061c8685974b678890d6ec41c0c746a]
    Former-commit-id: 6c610bd3569145652ce6f54b0a6fd4103d5bc681
    Former-commit-id: a48cb6fd6adea58b4391ce507245dee8f7663417

--- disaggregator/__init__.py
+++ disaggregator/__init__.py
@@ -1,5 +1,18 @@
+"""
+.. module:: disaggregator
+   :platform: Unix
+   :synopsis: A package for performing disaggregation on smart meter data.
+
+.. moduleauthor:: Phil Ngo <ngo.phil@gmail.com>
+.. moduleauthor:: Miguel Perez <miguel@invalid.com>
+.. moduleauthor:: Stephen Suffian <steve@invalid.com>
+.. moduleauthor:: Sabina Tomkins <sabina@invalid.com>
+"""
+
+# TODO remove these
 from appliance import *
 from utils import *
+
 import evaluation_metrics
 import PecanStreetDatasetAdapter
 from TracebaseDatasetAdapter import TracebaseDatasetAdapter
