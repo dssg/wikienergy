b9d1b50823a129eca48e887b91b909134e849d00
Miguel Perez <mperez4@saic.edu>
2014-07-11 14:42:54 -0500

    fixed bug
    Former-commit-id: bb01cc2f861712af13642cea0ac7d55c6b84700d
    Former-commit-id: 6545aa29f3dc0bafa8595e4520bf46cd99d484d3
    Former-commit-id: f85fac1c69e48dbabe80c231ab070f6c42412cc8

--- proto/vis_data/single_house_week.py
+++ proto/vis_data/single_house_week.py
@@ -5,20 +5,19 @@ import disaggregator as da
 import disaggregator.PecanStreetDatasetAdapter as psda
 import pickle
 
-
 db_url = "postgresql://USERNAME:PASSWORD@db.wiki-energy.org:5432/postgres"
 psda.set_url(db_url)
 
 schema = 'shared'
 table = 'validated_01_2014'
-dataid = 3893
+dataid = '3893'
 sample_rate = '15T'
 appliance_set = psda.generate_set_by_table_and_dataid(
-        schema, table, dataid, sample_rate)
+	schema, table, dataid, sample_rate)
 
 
 appliance_set = appliance_set.generate_non_zero_set()
 
 with open(os.path.join(os.pardir,os.pardir,'data','home_3893_set_01_2014.pkl'),'w') as f:
-    pickle.dump(appliance_set,f)
+	pickle.dump(appliance_set,f)
 
