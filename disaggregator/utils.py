7763d6a64816c1d14b41ef21a46001c5d14703b9
Phil Ngo <ngo.phil@gmail.com>
2014-07-15 17:28:16 -0500

    add draft of random appliance set generation
    Former-commit-id: 687e9ab557f5e32ef1c4b0db21516d11c17c9b86
    Former-commit-id: 21b2a8091470d3bf80d6afda0108ccf33721e60f
    Former-commit-id: b4829fe3293446ba8035354137782dc14b5d8ed8

--- disaggregator/utils.py
+++ disaggregator/utils.py
@@ -219,10 +219,31 @@ def pickle_object(obj,title):
     with open(os.path.join(rel_path,'data/{}.p'.format(title)),'wb') as f:
         pickle.dump(obj, f)
 
-def shuffle_appliance_sets(sets,other_params):
-    pass
+def generate_random_appliance_sets(appliance_sets,k,n):
+    """
+    Given a list of appliance sets, returns n randomly
+    sets, whose instances have been sampled
+    (w/replacement) from the instances of the given appliance_sets.
+
+    Appliance sets must be aligned.
+    """
+    all_instances = [instance for appliance_set in appliance_sets
+                     for instance in appliance_set.instances]
+    n_instances = len(all_instances)
+    all_sets = []
+    for _ in xrange(n):
+        instances = []
+        for _ in xrange(k):
+            instance = all_instances[random.randrange(n_instances)]
+            instances.append(instance)
+        appliance_set = ApplianceSet(instances)
+        all_sets.append(appliance_set)
+    return all_sets
 
 def get_trace_windows(trace,window_length,window_step):
+    """
+    Returns a nump array with stacked sliding windows of data from a trace.
+    """
     total_length = trace.series.size
     n_steps = int((total_length - window_length) / window_step)
     windows = []
