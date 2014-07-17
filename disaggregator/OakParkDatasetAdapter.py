520177dd5afdf6572d6f5b8fadecfd3df5906d29
Sabina321 <sabina@Sabinas-MacBook-Pro.local>
2014-07-17 12:47:06 -0500

    going to make a smaller method in a sec
    Former-commit-id: f8abab4f6bdeee6b095594fc73d55274fc57033c
    Former-commit-id: 1e0ffa12be93d5140ea80acc052093201b561150
    Former-commit-id: 22e94c16bc127d363d064ec1f54edbf38510f6f6

--- disaggregator/OakParkDatasetAdapter.py
+++ disaggregator/OakParkDatasetAdapter.py
@@ -47,7 +47,42 @@ def generate_trace_by_dataid(homes,dataid):
     house = homes[dataid]
     dates = [x[0] for x in house['interval_readings']]
     values = [x[1]['value'] for x in house['interval_readings']]
-    return pd.Series(values, index = dates)
+    
+    return ApplianceTrace(pd.Series(values, index = dates),house['meta'])
 
+def resample_trace_by_month(trace,month):
+    '''
+    Returns a set of houses for a given month.
+    '''
+    return utils.resample_trace(trace,'MS')
+
+def get_list_of_homes_with_certain_month_year(homes,year,month):
+    '''
+        Returns a list of homes which have complete trace info for given year and month.
+        Assumes that homes is dict where keys are dataids and values are traces.
+        '''
+    complete_homes = []
+    for h in homes.keys():
+        dt_start = datetime(year,month,1)
+        dt_end = datetime(year,month,calendar.monthrange(year,month)[1])
+        index_start = homes[h].series.index.searchsorted(dt_start)
+        index_end = homes[h].series.index.searchsorted(dt_end)
+        if index_end-index_start==((dt_end-dt_start)*48).days:
+            complete_homes.append(h)
+    return complete_homes
+
+def get_home_series_by_year_month(year,month,trace):
+    '''
+        Returns the home's series sliced by year and month.
+        '''
+    dt_start = datetime(year,month,1)
+    dt_end = datetime(year,month,calendar.monthrange(year,month)[1])
+    index_start = trace.series.index.searchsorted(dt_start)
+    index_end = trace.series.index.searchsorted(dt_end)
+    
+    if index_start == 0 or index_end==0:
+        return -1
+    else:
+        return  trace.series[index_start:index_end]
 
 
