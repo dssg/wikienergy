45c0f3709967964e3ba3ec0555ade1c4f0b00d61
Phil Ngo <ngo.phil@gmail.com>
2014-07-21 14:19:52 -0500

    update pymc model
    Former-commit-id: fc9b306c72134f1c81824879ccf0e075fe567502
    Former-commit-id: f8b24a3b0c49c133d24faf71f5431caaa3ee4e33

--- proto/pymc_models/hmm_model.py
+++ proto/pymc_models/hmm_model.py
@@ -1,7 +1,48 @@
-from pymc import Gamma, Normal, Dirichlet
+import pymc
+import numpy as np
 
-mean1 = Gamma("mean1",alpha=0.1,beta=1)
-mean2 = Gamma("mean1",alpha=100,beta=1)
-mean3 = Gamma("mean1",alpha=200,beta=1)
 
-covar1 = None
+trans = [[80,10],[10,80],[10,10]]
+n_samples = 1000
+means = [pymc.rgamma(alpha,beta,size=n_samples) for alpha,beta in zip([1,2,3],[0.1,0.2,0.3])]
+variances = [pymc.rgamma(alpha,beta,size=n_samples) for alpha,beta in zip([.2,.3,.4],[0.1,0.1,0.1])]
+transitions = [pymc.rdirichlet(trans_,size=n_samples) for trans_ in trans]
+
+
+n_gamma = 3
+n_modes = n_gamma * 2
+mean_params = [pymc.Gamma('mean_param{}'.format(i), alpha=1, beta=.1) for i in range(n_modes)]
+var_params = [pymc.Gamma('var_param{}'.format(i), alpha=1, beta=.1) for i in range(n_modes)]
+trans_params = [pymc.Beta('trans_params{}'.format(i), alpha=1,beta=1) for i in range(n_modes)]
+
+
+mean_obs = []
+mean_pred = []
+var_obs = []
+var_pred = []
+trans_obs = []
+trans_pred = []
+
+for i in xrange(n_gamma):
+    alpha1 = mean_params[i*2]
+    beta1 = mean_params[i*2+1]
+    mean_obs.append(pymc.Gamma("mean_obs{}".format(i),alpha=alpha1,beta=beta1,value=means[i],observed=True))
+    mean_pred.append(pymc.Gamma("mean_pred{}".format(i),alpha=alpha1,beta=beta1))
+
+    alpha2 = var_params[i*2]
+    beta2 = var_params[i*2+1]
+    var_obs.append(pymc.Gamma("var_obs{}".format(i),alpha=alpha2,beta=beta2,value=variances[i],observed=True))
+    var_pred.append(pymc.Gamma("var_pred{}".format(i),alpha=alpha2,beta=beta2))
+
+    prob1 = trans_params[i*2]
+    prob2 = trans_params[i*2+1]
+    @pymc.deterministic
+    def params(a=prob1,b=prob2):
+        return np.array([a,b])
+    trans_obs.append(pymc.Dirichlet("trans_obs{}".format(i),params,value=transitions[i],observed=True))
+    trans_pred.append(pymc.Dirichlet("trans_pred_incomplete{}".format(i),params))
+
+pred = mean_pred[:]
+pred.extend(var_pred)
+pred.extend(trans_pred)
+model = pymc.Model(pred)
