78a79659a310175a66a981fe51bc27ce22adda92
Phil Ngo <ngo.phil@gmail.com>
2014-07-21 16:35:21 -0500

    import appliance models and generate distributions
    Former-commit-id: ad065641add8b3abd92a48df62243eac7f82966c
    Former-commit-id: 0f0c1399ef2a8fba91285f396f4a496026d5b119
    Former-commit-id: 2fff5b945c9a71eb10bcd8fd220b0f6c1743bb13

--- proto/pymc_models/hmm_model.py
+++ proto/pymc_models/hmm_model.py
@@ -2,7 +2,7 @@ import pymc
 import numpy as np
 
 
-trans = [[80,10],[10,80],[10,10]]
+trans = [[80,10,10],[10,80,10],[10,10,80]]
 n_samples = 1000
 means = [pymc.rgamma(alpha,beta,size=n_samples) for alpha,beta in zip([1,2,3],[0.1,0.2,0.3])]
 variances = [pymc.rgamma(alpha,beta,size=n_samples) for alpha,beta in zip([.2,.3,.4],[0.1,0.1,0.1])]
@@ -13,7 +13,7 @@ n_gamma = 3
 n_modes = n_gamma * 2
 mean_params = [pymc.Gamma('mean_param{}'.format(i), alpha=1, beta=.1) for i in range(n_modes)]
 var_params = [pymc.Gamma('var_param{}'.format(i), alpha=1, beta=.1) for i in range(n_modes)]
-trans_params = [pymc.Beta('trans_params{}'.format(i), alpha=1,beta=1) for i in range(n_modes)]
+trans_params = [pymc.Beta('trans_params{}'.format(i), alpha=1,beta=1) for i in range(n_gamma*n_gamma)]
 
 
 mean_obs = []
@@ -34,11 +34,12 @@ for i in xrange(n_gamma):
     var_obs.append(pymc.Gamma("var_obs{}".format(i),alpha=alpha2,beta=beta2,value=variances[i],observed=True))
     var_pred.append(pymc.Gamma("var_pred{}".format(i),alpha=alpha2,beta=beta2))
 
-    prob1 = trans_params[i*2]
-    prob2 = trans_params[i*2+1]
+    prob1 = trans_params[i*3]
+    prob2 = trans_params[i*3+1]
+    prob3 = trans_params[i*3+2]
     @pymc.deterministic
-    def params(a=prob1,b=prob2):
-        return np.array([a,b])
+    def params(a=prob1,b=prob2,c=prob3):
+        return np.array([a,b,c])
     trans_obs.append(pymc.Dirichlet("trans_obs{}".format(i),params,value=transitions[i],observed=True))
     trans_pred.append(pymc.Dirichlet("trans_pred_incomplete{}".format(i),params))
 
