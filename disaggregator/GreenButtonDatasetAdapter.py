45e554e9007cb7081f7369baa1bc33553516d05a
Phil Ngo <ngo.phil@gmail.com>
2014-07-28 15:54:59 -0500

    add green button dataset adapter draft
    Former-commit-id: 86b44ae8b472706563d5e7d717d2d78c2d2b7976 [formerly 30445c91400a5b817e21ee6d5ba864d18db2cf5e] [formerly 3e95349df2ad9917debfb63350c50557f07b59ad]
    Former-commit-id: 2611ae7ba4cf9c28e4bc41bbb0dbe798126e2c09
    Former-commit-id: a6ec4776e139e7d02e7fad6cff205a5d9e662e07

---
+++ disaggregator/GreenButtonDatasetAdapter.py
@@ -0,0 +1,69 @@
+"""
+.. module:: GreenButtonDatasetAdapter
+   :platform: Unix
+   :synopsis: Contains methods for importing data following the green button
+   spec.
+
+.. moduleauthor:: Phil Ngo <ngo.phil@gmail.com>
+.. moduleauthor:: Miguel Perez <miguel@invalid.com>
+.. moduleauthor:: Stephen Suffian <steve@invalid.com>
+.. moduleauthor:: Sabina Tomkins <sabina.tomkins@gmail.com>
+
+"""
+
+
+from appliance import ApplianceTrace
+from appliance import ApplianceInstance
+from appliance import ApplianceSet
+from appliance import ApplianceType
+
+import utils
+
+import sqlalchemy
+import pandas as pd
+import numpy as np
+
+from lxml import etree
+
+
+
+def get_trace_from_xml(xml_filepath):
+    '''
+    This function returns a unique set of dates (corresponding to
+    individual files) for a single device instance id
+    '''
+    schema_file = ''
+    with open(schema_file, 'r') as f:
+        schema_root = etree.XML(f.read())
+    schema = etree.XMLSchema(schema_root)
+    xmlparser = etree.XMLParser(schema=schema)
+
+    with open(xml_filepath,'r') as f:
+        xml_string = f.read()
+
+    if not _validate(xml_string):
+        raise InvalidXMLError
+
+    pass
+
+def _validate(xml_string):
+    try:
+        etree.fromstring(xml_string, xmlparser)
+        return True
+    except:
+        return False
+
+
+class InvalidXMLError(Exception):
+    """
+
+    Exception raised for errors in the xml format of the data.
+
+    """
+    def __init__(self):
+        pass
+
+    def __str__(self):
+
+        return '''Improperly formed XML file. Please make sure the file follows
+            the green button specification. '''
