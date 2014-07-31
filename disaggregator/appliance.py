b5f828deec36ae9a372a5bcad15d0d826de5deee
Phil Ngo <ngo.phil@gmail.com>
2014-07-31 10:59:44 -0500

    test disaggregation, discovered unpickle from gpu to cpu problem
    Former-commit-id: 687e8942ccdf395ed4291b09e2d482b3ef201bcf
    Former-commit-id: 97e5b6e2803cd9fdc83fee331e867c907c7323ad

--- disaggregator/appliance.py
+++ disaggregator/appliance.py
@@ -120,8 +120,8 @@ class ApplianceTrace(object):
         '''
         data = []
         for i, v in self.series.iteritems():
-            data.append({'date':i.strftime('%Y-%m-%d %H:%M'),                         
-                         'value': float(v)})
+            data.append({'date':i.strftime('%Y-%m-%d %H:%M'),
+                         'reading': float(v)})
 
         json_string = json.dumps(data, ensure_ascii=False,
                                  indent=4, separators=(',', ': '))
