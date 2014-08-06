2c375076a3853f535600fcc672fd4a1dd8bcae33
Phil Ngo <ngo.phil@gmail.com>
2014-08-05 19:38:51 -0500

    add concatenate trace method and tests for it
    Former-commit-id: 264c10f131c15bbb05c6679df14a01b127805d15
    Former-commit-id: 8f34618c517ad8edfc570a6a45a3dd51f4051f1c

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -39,19 +39,28 @@ class UtilsTestCase(unittest.TestCase):
         [self.assertNotIn(el,common3) for el in [1,7]]
 
     def test_aggregate_traces_misaligned(self):
-        self.assertRaises(da.AlignmentError,da.aggregate_traces,self.consecutive_traces,{})
+        self.assertRaises(da.AlignmentError,
+                          da.aggregate_traces,self.consecutive_traces,{})
 
     def test_aggregate_traces_aligned(self):
         aligned_traces = da.align_traces(self.consecutive_traces)
         da.aggregate_traces(aligned_traces,{})
 
     def test_aggregate_instances_misaligned(self):
-        self.assertRaises(da.AlignmentError,da.aggregate_instances,self.consecutive_instances,{})
+        self.assertRaises(da.AlignmentError,
+                          da.aggregate_instances,self.consecutive_instances,{})
 
     def test_aggregate_instances_aligned(self):
         aligned_instances = da.align_instances(self.consecutive_instances)
         da.aggregate_instances(aligned_instances,{})
 
+    def test_concatenate_traces_consecutive(self):
+        trace = da.concatenate_traces(self.consecutive_traces,{})
+        self.assertEqual(trace.series.index.size, 24 * 4 * 5)
+
+    def test_concatenate_instances_consecutive(self):
+        instance = da.concatenate_instances(self.consecutive_instances,{})
+        self.assertEqual(instance.traces[0].series.index.size, 24 * 4 * 5)
 
 if __name__ == "__main__":
     unittest.main()
