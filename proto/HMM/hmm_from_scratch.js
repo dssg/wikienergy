a2a24945fdb1bec817373ccea15905800621c649
Phil Ngo <ngo.phil@gmail.com>
2014-08-18 14:00:05 -0500

    refactor settings
    Former-commit-id: 71f5ca88604daa32000dfa0d4437c9f313e2e3af
    Former-commit-id: dbc715a2bb2b4a39081a5ff33df9cf8595c555ff

---
+++ proto/HMM/hmm_from_scratch.js
@@ -0,0 +1,35 @@
+require("sylvester");
+
+var observations = [0.,30.,50.,100.,100.,75.,200.,20.,10.,0.];
+
+var n_states = 3;
+
+var mus = [0.,100.,50.];
+var sigmas = [20.,20.,20.];
+
+var start_prob = [.33,.33,.34];
+var start_prob = start_prob.map(function(x, i) {
+    return - Math.log(x);
+});
+var trans_prob = [[.9,.05,.05],[.1,.8,.1],[.2,.2,.6]];
+var trans_prob = trans_prob.map(function(x, i) {
+    return - Math.log(x);
+});
+
+function norm_pdf(X,mu,sigma) {
+    return X.map(function(x,i) {
+        return -Math.log(Math.exp(-(x-mu)^2/2/(sigma^2))/Math.sqrt(2*Math.PI)/sigma);
+    });
+}
+
+function state_probabilities(observations,mus,sigmas) {
+    var state_probs = [];
+    for (i = 0; i < n_states; i++) {
+        state_probs.push(norm_pdf(observations,mus[i],sigmas[i]));
+    }
+    return state_probs;
+}
+
+state_probs = state_probabilities(observations,mus,sigmas);
+
+console.log(state_probs)
