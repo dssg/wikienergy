37842f8006c760030f1a5e6384c84ef917ddab43
Stephen Suffian <stephen.suffian@gmail.com>
2014-08-19 10:23:41 -0500

    updated solar.py
    Former-commit-id: d56bca536ec334deef2c4d9b91962a02d0e233e1
    Former-commit-id: f802f3468c55c70cd93ee6736e7210d0da814b5f

--- disaggregator/solar.py
+++ disaggregator/solar.py
@@ -5,56 +5,54 @@ import datetime
 
 def get_solar_data_from_nrel(api,zip_code):
     f = urllib2.urlopen('http://developer.nrel.gov/api/solar/solar_resource/'
-           +'v1.json?api_key='+api+'&address='+zip_code)
+           +'v1.json?api_key='+str(api)+'&address='+str(zip_code))
     json_string = f.read()
     parsed_json = json.loads(json_string)
     return parsed_json['outputs']['avg_lat_tilt']['monthly']
 
-def get_month_solar_data(month_num,month_data):
+def get_month_name(month_num):
     if month_num is 1:
-        return month_data['jan']
+        return 'jan'
     elif month_num is 2:
-        return month_data['feb']
+        return 'feb'
     elif month_num is 3:
-        return month_data['mar']
+        return 'mar'
     elif month_num is 4:
-        return month_data['apr']
+        return 'apr'
     elif month_num is 5:
-        return month_data['may']
+        return 'may'
     elif month_num is 6:
-        return month_data['jun']
+        return 'jun'
     elif month_num is 7:
-        return month_data['jul']
+        return 'jul'
     elif month_num is 8:
-        return month_data['aug']
+        return 'aug'
     elif month_num is 9:
-        return month_data['sep']
+        return 'sep'
     elif month_num is 10:
-        return month_data['oct']
+        return 'oct'
     elif month_num is 11:
-        return month_data['nov']
+        return 'nov'
     elif month_num is 12:
-        return month_data['dec']
+        return 'dec'
     else:
         raise ValueError("invalid month number.")
 
-def calculate_solar_generated(start_dt,end_dt,pv_size,api,zip_code,eff_factor=0.8):
 
-    month_data=get_solar_data_from_nrel(api,zip_code)
+def calculate_solar_generated(start_dt,end_dt,api,zip_code,month_data,eff_factor=0.8):
+
+    month_data = get_solar_data_from_nrel(api,zip_code)
     delta = end_dt - start_dt
-
+    data = []
     this_month = start_dt.month
     total_kWh = 0
     for val in range(delta.days+1):
         delta_days=datetime.timedelta(val)
-        total_kWh = total_kWh + get_month_solar_data(this_month,
-                month_data)*pv_size*eff_factor
+        month_name= get_month_name(this_month)
+        total_kWh=total_kWh+month_data[month_name]*eff_factor
         if (start_dt+delta_days).month is not this_month:
+            data.append({'date':str(month_name.title())+' ' + str((start_dt+delta_days).year) ,'value':total_kWh/float(1000)})
             this_month=(start_dt+delta_days).month
 
-    data = []
-    kwh = v/1000
-    data.append({'solar_savings':total_kWh/float(1000)})
     json_string = json.dumps(data, ensure_ascii=False)
     return json_string
-
