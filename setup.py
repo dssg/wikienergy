59a6d2ac261fbd0ee772591eb8523acacdddfeaa
Stephen Suffian <stephen.suffian@gmail.com>
2014-08-21 16:03:58 -0500

    added setup.py to install our package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='disaggregator',
+      version='1.0',
+      description='Energy Disaggregator Module',
+      author='Sabina Tomkins, Miguel Perez, Phil Ngo, Stephen Suffian',
+      url='http://www.smartenergyactions.org',
+      packages=['disaggregator'],
+      )
